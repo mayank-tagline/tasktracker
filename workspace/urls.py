4d4c128116bb45f664fca5c117d673d7a08d5bdc
mayank-tagline <mayankdri.tagline@gmail.com>
2026-04-17 17:49:48 +0530

    Add workspace detail view and template, update URLs for workspace navigation

--- workspace/urls.py
+++ workspace/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import WorkSpaceView
 from .views import WorkSpaceCreateView
+from .views import WorkSpaceDetailView
 
 urlpatterns = [
     path('workspace/', WorkSpaceView.as_view() , name='workspace'),
     path('workspace/add', WorkSpaceCreateView.as_view() , name='add-workspace'),
+    path('workspace/<str:workspace_id>', WorkSpaceDetailView.as_view() , name='workspace-detail'),
 ]
--- workspace/views.py
+++ workspace/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.views import View
+
+from task.models import Task
 from .models import WorkSpace
 from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -31,3 +33,15 @@ class WorkSpaceCreateView(LoginRequiredMixin, View):
         workspace.save()
         return redirect('workspace')
 
+class WorkSpaceDetailView(LoginRequiredMixin, View):
+    login_url = '/login/'
+    redirect_field_name = ''
+    def get(self, request, workspace_id):
+        templates = loader.get_template('workspace_detail.html')
+        workspace = WorkSpace.objects.get(id=workspace_id)
+        tasks = Task.objects.filter(workspace=workspace)
+        context = {
+            'workspace': workspace,
+            'tasks': tasks,
+        }
+        return HttpResponse(templates.render(context, request))
