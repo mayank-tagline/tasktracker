13745369a128a3c707ac6dd3db05940d8718c29b
mayank-tagline <mayankdri.tagline@gmail.com>
2026-04-27 17:01:00 +0530

    Remove unnecessary blank line in account URL patterns and enable CSRF middleware in settings

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import LoginView, RegisterView, LogoutView, UserDetailView, UserView
 
-
 urlpatterns = [
     path('login/', LoginView.as_view(redirect_authenticated_user=True) , name='login'),
     path('register/', RegisterView.as_view() , name='register'),
